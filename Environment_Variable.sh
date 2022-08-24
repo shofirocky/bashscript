ac07609aceb0139805aa32b551a20d097f8525f8
Shofi Ahmed Rocky <27762830+shofirocky@users.noreply.github.com>
2022-08-24 22:34:12 +0600

    Add files via upload

---
+++ Environment_Variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+name="Rocky"
+
+user=$(whoami)
+date=$(date)
+whereami=$(pwd)
+
+echo "I am $Rocky"
+
+echo "I am logged in $user and I am in $whereami directory ; Date of Today is $date"
